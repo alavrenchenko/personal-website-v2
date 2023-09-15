e5cc61ecd57f919375ff1c44a15aa5a6bb828fe0
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-09-15 13:49:54 +0300

    api-clients.config: replace AppManagerServiceClientConfig, LoggingManagerServiceClientConfig with ServiceClientConfig

--- api-clients/config/config.go
+++ api-clients/config/config.go
@@ -1,12 +1,7 @@
 package config
 
-type AppManagerServiceClientConfig struct {
-	ServerAddr  string `json:"serverAddr"`
-	DialTimeout int64  `json:"dialTimeout"` // in milliseconds
-	CallTimeout int64  `json:"callTimeout"` // in milliseconds
-}
-
-type LoggingManagerServiceClientConfig struct {
+// AppManagerService, LoggingManagerService.
+type ServiceClientConfig struct {
 	ServerAddr  string `json:"serverAddr"`
 	DialTimeout int64  `json:"dialTimeout"` // in milliseconds
 	CallTimeout int64  `json:"callTimeout"` // in milliseconds
