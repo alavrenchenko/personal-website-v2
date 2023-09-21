ab3fa6bb148c14e9f138d3931496a1a5dda9ad58
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-09-22 00:11:03 +0300

    app-manager.test.app: add Application_logLoggingError tests

--- app-manager/src/test/app/test.go
+++ app-manager/src/test/app/test.go
@@ -29,6 +29,8 @@ import (
 	"personal-website-v2/pkg/base/nullable"
 	"personal-website-v2/pkg/logging"
 	lcontext "personal-website-v2/pkg/logging/context"
+	grpcserver "personal-website-v2/pkg/net/grpc/server"
+	httpserver "personal-website-v2/pkg/net/http/server"
 	actionhelper "personal-website-v2/test/helper/actions"
 	logginghelper "personal-website-v2/test/helper/logging"
 )
@@ -78,21 +80,21 @@ func exec() {
 		panic(err)
 	}
 
-	o, err := a.Operations.CreateAndStart(0, actions.OperationCategoryCommon, actions.OperationGroupNoGroup, uuid.NullUUID{})
+	o, err := a.Operations.CreateAndStart(0, actions.OperationCategoryCommon, actions.OperationGroupNoGroup, uuid.NullUUID{},
+		actions.NewOperationParam("param1", "test"),
+		actions.NewOperationParam("param2", 10),
+	)
 	if err != nil {
 		panic(err)
 	}
 
-	succeeded := false
-	defer func() {
-		if err := a.Operations.Complete(o, succeeded); err != nil {
-			panic(err)
-		}
+	if err := a.Operations.Complete(o, true); err != nil {
+		panic(err)
+	}
 
-		if err := actionManager.Complete(a, succeeded); err != nil {
-			panic(err)
-		}
-	}()
+	if err := actionManager.Complete(a, true); err != nil {
+		panic(err)
+	}
 
 	opCtx := actions.NewOperationContext(context.Background(), appSessionId, t, a, o)
 	opCtx.UserId = nullable.NewNullable(userId)
@@ -101,8 +103,25 @@ func exec() {
 	testApplication_onFileLoggingError(leCtx)
 
 	fmt.Println()
+	testApplication_logLoggingError_WhenEntryIsLogEntry(leCtx)
 
-	succeeded = true
+	fmt.Println()
+	testApplication_logLoggingError_WhenEntryIsTransaction(t)
+
+	fmt.Println()
+	testApplication_logLoggingError_WhenEntryIsAction(a)
+
+	fmt.Println()
+	testApplication_logLoggingError_WhenEntryIsOperation(o)
+
+	fmt.Println()
+	testApplication_logLoggingError_WhenEntryIsRequest()
+
+	fmt.Println()
+	testApplication_logLoggingError_WhenEntryIsResponse()
+
+	fmt.Println()
+	testApplication_logLoggingError_WhenEntryIsCall()
 }
 
 func testApplication_onFileLoggingError(ctx *lcontext.LogEntryContext) {
@@ -120,4 +139,98 @@ func testApplication_onFileLoggingError(ctx *lcontext.LogEntryContext) {
 	err := logging.NewLoggingError("error", []error{errors.New("error 1"), errors.New("error 2")})
 
 	app.TestApplication_onFileLoggingError(entry, err)
+}
+
+func testApplication_logLoggingError_WhenEntryIsLogEntry(ctx *lcontext.LogEntryContext) {
+	entry := &logging.LogEntry[*lcontext.LogEntryContext]{
+		Id:        uuid.New(),
+		Timestamp: time.Now(),
+		Context:   ctx,
+		Level:     logging.LogLevelError,
+		Category:  "test.app",
+		Event:     logging.NewEvent(0, "testApplication_logLoggingError", logging.EventCategoryCommon, logging.EventGroupNoGroup),
+		Err:       errors.New("error"),
+		Message:   "message",
+		Fields:    []*logging.Field{{Key: "key1", Value: "test"}, {Key: "key2", Value: 10}},
+	}
+	err := logging.NewLoggingError("error", []error{errors.New("error 1"), errors.New("error 2")})
+
+	app.TestApplication_logLoggingError(entry, err)
+}
+
+func testApplication_logLoggingError_WhenEntryIsTransaction(t *actions.Transaction) {
+	err := logging.NewLoggingError("error", []error{errors.New("error 1"), errors.New("error 2")})
+
+	app.TestApplication_logLoggingError(t, err)
+}
+
+func testApplication_logLoggingError_WhenEntryIsAction(a *actions.Action) {
+	err := logging.NewLoggingError("error", []error{errors.New("error 1"), errors.New("error 2")})
+
+	app.TestApplication_logLoggingError(a, err)
+}
+
+func testApplication_logLoggingError_WhenEntryIsOperation(o *actions.Operation) {
+	err := logging.NewLoggingError("error", []error{errors.New("error 1"), errors.New("error 2")})
+
+	app.TestApplication_logLoggingError(o, err)
+}
+
+func testApplication_logLoggingError_WhenEntryIsRequest() {
+	req := &httpserver.RequestInfo{
+		Id:             uuid.New(),
+		Status:         httpserver.RequestStatusSuccess,
+		StartTime:      time.Now(),
+		EndTime:        nullable.NewNullable(time.Now()),
+		ElapsedTime:    nullable.NewNullable(100 * time.Microsecond),
+		Url:            "url",
+		Method:         "GET",
+		Protocol:       "http",
+		Host:           "host",
+		RemoteAddr:     "addr",
+		RequestURI:     "uri",
+		ContentLength:  100,
+		ContentType:    "",
+		UserAgent:      "user_agent",
+		Referer:        "",
+		Origin:         "origin",
+		Accept:         "",
+		AcceptEncoding: "",
+		AcceptLanguage: "accept_language",
+	}
+	err := logging.NewLoggingError("error", []error{errors.New("error 1"), errors.New("error 2")})
+
+	app.TestApplication_logLoggingError(req, err)
+}
+
+func testApplication_logLoggingError_WhenEntryIsResponse() {
+	res := &httpserver.ResponseInfo{
+		Id:          uuid.New(),
+		RequestId:   uuid.New(),
+		Timestamp:   time.Now(),
+		StatusCode:  200,
+		BodySize:    100,
+		ContentType: "content_type",
+	}
+	err := logging.NewLoggingError("error", []error{errors.New("error 1"), errors.New("error 2")})
+
+	app.TestApplication_logLoggingError(res, err)
+}
+
+func testApplication_logLoggingError_WhenEntryIsCall() {
+	call := &grpcserver.CallInfo{
+		Id:                    uuid.New(),
+		Status:                grpcserver.CallStatusSuccess,
+		StartTime:             time.Now(),
+		EndTime:               nullable.NewNullable(time.Now()),
+		ElapsedTime:           nullable.NewNullable(100 * time.Microsecond),
+		FullMethod:            "method",
+		ContentType:           []string{"content_type"},
+		UserAgent:             []string{"user_agent"},
+		IsOperationSuccessful: nullable.NewNullable(true),
+		StatusCode:            nullable.NewNullable[uint32](0),
+	}
+	err := logging.NewLoggingError("error", []error{errors.New("error 1"), errors.New("error 2")})
+
+	app.TestApplication_logLoggingError(call, err)
 }
