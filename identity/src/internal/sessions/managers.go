acca80acc5bc7c6c3ce66e5af9f042930099756c
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-10-11 20:00:37 +0300

    identity.internal.sessions: add CreateAndStart, Terminate, FindById to UserSessionManager (interface)

--- identity/src/internal/sessions/managers.go
+++ identity/src/internal/sessions/managers.go
@@ -14,8 +14,22 @@
 
 package sessions
 
+import (
+	"personal-website-v2/identity/src/internal/sessions/dbmodels"
+	"personal-website-v2/pkg/actions"
+)
+
 // UserSessionManager is a user's session manager.
 type UserSessionManager interface {
+	// CreateAndStart creates and starts a user's session for the specified user
+	// and returns user's session ID if the operation is successful.
+	CreateAndStart(ctx *actions.OperationContext, userId uint64) (uint64, error)
+
+	// Terminate terminates a user's session by the specified user session ID.
+	Terminate(ctx *actions.OperationContext, id uint64) error
+
+	// FindById finds and returns user's session info, if any, by the specified user session ID.
+	FindById(ctx *actions.OperationContext, id uint64) (*dbmodels.UserSessionInfo, error)
 }
 
 // UserAgentSessionManager is a user's agent session manager.
