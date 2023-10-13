37fed6a198f28b15968e1975215fb39d8acbc08e
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-10-13 17:44:02 +0300

    identity.internal.errors: add errors

--- identity/src/internal/errors/errors.go
+++ identity/src/internal/errors/errors.go
@@ -30,9 +30,24 @@ const (
 	ErrorCodeClientNotFound  errors.ErrorCode = 31200
 	ErrorCodeInvalidClientId errors.ErrorCode = 31201
 
-	// User session error codes (31400-31599).
-	ErrorCodeUserSessionNotFound  errors.ErrorCode = 31400
-	ErrorCodeInvalidUserSessionId errors.ErrorCode = 31401
+	// User agent error codes (31400-31599).
+	ErrorCodeUserAgentNotFound  errors.ErrorCode = 31400
+	ErrorCodeInvalidUserAgentId errors.ErrorCode = 31401
+
+	// Authentication error codes (31600-31799).
+	// Authorization error codes (31800-31999).
+	// Permission error codes (32000-32199).
+
+	// User session error codes (32200-32399).
+	ErrorCodeUserSessionNotFound  errors.ErrorCode = 32200
+	ErrorCodeInvalidUserSessionId errors.ErrorCode = 32201
+
+	// User agent session error codes (32400-32599).
+	ErrorCodeUserAgentSessionNotFound  errors.ErrorCode = 32400
+	ErrorCodeInvalidUserAgentSessionId errors.ErrorCode = 32401
+
+	// Authentication token encryption key error codes (32600-32799).
+	ErrorCodeAuthTokenEncryptionKeyNotFound errors.ErrorCode = 32600
 )
 
 var (
@@ -44,7 +59,22 @@ var (
 	ErrClientNotFound  = errors.NewError(ErrorCodeClientNotFound, "client not found")
 	ErrInvalidClientId = errors.NewError(ErrorCodeInvalidClientId, "invalid client id")
 
+	// User agent errors.
+	ErrUserAgentNotFound  = errors.NewError(ErrorCodeUserAgentNotFound, "user's agent not found")
+	ErrInvalidUserAgentId = errors.NewError(ErrorCodeInvalidUserAgentId, "invalid user agent id")
+
+	// Authentication errors.
+	// Authorization errors.
+	// Permission errors.
+
 	// User session errors.
 	ErrUserSessionNotFound  = errors.NewError(ErrorCodeUserSessionNotFound, "user's session not found")
 	ErrInvalidUserSessionId = errors.NewError(ErrorCodeInvalidUserSessionId, "invalid user session id")
+
+	// User agent session errors.
+	ErrUserAgentSessionNotFound  = errors.NewError(ErrorCodeUserAgentSessionNotFound, "user's agent session not found")
+	ErrInvalidUserAgentSessionId = errors.NewError(ErrorCodeInvalidUserAgentSessionId, "invalid user agent session id")
+
+	// Authentication token encryption key errors.
+	ErrAuthTokenEncryptionKeyNotFound = errors.NewError(ErrorCodeAuthTokenEncryptionKeyNotFound, "authentication token encryption key not found")
 )
