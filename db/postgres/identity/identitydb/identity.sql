4fa7e9595c1b855e2a2f657061f497a0ed8c7544
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-10-17 16:39:19 +0300

    db.postgres.identity: add permissions, permission_groups to identitydb

--- db/postgres/identity/identitydb/identity.sql
+++ db/postgres/identity/identitydb/identity.sql
@@ -23,3 +23,68 @@ CREATE DATABASE identity
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
+
+
+-- Table: public.permission_groups
+/*
+Permission group statuses:
+    Unspecified = 0
+    New         = 1
+    Active      = 2
+    Inactive    = 3
+    Deleted     = 4
+*/
+CREATE TABLE IF NOT EXISTS public.permission_groups
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    name character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    created_by bigint NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL DEFAULT (clock_timestamp() AT TIME ZONE 'UTC'::text),
+    updated_by bigint NOT NULL,
+    status smallint NOT NULL,
+    status_updated_at timestamp(6) without time zone NOT NULL DEFAULT (clock_timestamp() AT TIME ZONE 'UTC'::text),
+    status_updated_by bigint NOT NULL,
+    status_comment text COLLATE pg_catalog."default",
+    app_id bigint,
+    description text COLLATE pg_catalog."default" NOT NULL,
+    _version_stamp bigint NOT NULL,
+    _timestamp timestamp(6) without time zone NOT NULL DEFAULT (clock_timestamp() AT TIME ZONE 'UTC'::text),
+    CONSTRAINT permission_groups_pkey PRIMARY KEY (id),
+    CONSTRAINT permission_groups_name_key UNIQUE (name)
+)
+TABLESPACE pg_default;
+
+-- Table: public.permissions
+/*
+Permission statuses:
+    Unspecified = 0
+    New         = 1
+    Active      = 2
+    Inactive    = 3
+    Deleted     = 4
+*/
+CREATE TABLE IF NOT EXISTS public.permissions
+(
+    id bigint NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    group_id bigint NOT NULL,
+    name character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    created_at timestamp(6) without time zone NOT NULL,
+    created_by bigint NOT NULL,
+    updated_at timestamp(6) without time zone NOT NULL DEFAULT (clock_timestamp() AT TIME ZONE 'UTC'::text),
+    updated_by bigint NOT NULL,
+    status smallint NOT NULL,
+    status_updated_at timestamp(6) without time zone NOT NULL DEFAULT (clock_timestamp() AT TIME ZONE 'UTC'::text),
+    status_updated_by bigint NOT NULL,
+    status_comment text COLLATE pg_catalog."default",
+    description text COLLATE pg_catalog."default" NOT NULL,
+    _version_stamp bigint NOT NULL,
+    _timestamp timestamp(6) without time zone NOT NULL DEFAULT (clock_timestamp() AT TIME ZONE 'UTC'::text),
+    CONSTRAINT permissions_pkey PRIMARY KEY (id),
+    CONSTRAINT permissions_name_key UNIQUE (name),
+    CONSTRAINT permissions_group_id_fkey FOREIGN KEY (group_id)
+        REFERENCES public.permission_groups (id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+)
+TABLESPACE pg_default;
