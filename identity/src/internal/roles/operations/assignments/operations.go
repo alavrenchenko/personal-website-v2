d444a5d66e4edcfbf9127c2e7fba94c8dc16af68
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-10-20 21:35:16 +0300

    identity.internal.roles.operations: add assignments, userroleassignments

---
+++ identity/src/internal/roles/operations/assignments/operations.go
@@ -0,0 +1,31 @@
+// Copyright 2023 Alexey Lavrenchenko. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package assignments
+
+import (
+	"personal-website-v2/identity/src/internal/roles/models"
+)
+
+type CreateOperationData struct {
+	// The role ID.
+	RoleId uint64 `json:"roleId"`
+
+	// The unique ID of the entity the role is assigned to - either the userId of a user
+	// or the groupId of a group.
+	AssignedTo uint64 `json:"assignedTo"`
+
+	// The type of the assignee.
+	AssigneeType models.AssigneeType `json:"assigneeType"`
+}
---
+++ identity/src/internal/roles/operations/userroleassignments/operations.go
@@ -0,0 +1,26 @@
+// Copyright 2023 Alexey Lavrenchenko. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package userroleassignments
+
+type CreateOperationData struct {
+	// The user ID.
+	UserId uint64 `json:"userId"`
+
+	// The role ID.
+	RoleId uint64 `json:"roleId"`
+
+	// The role assignment ID.
+	RoleAssignmentId uint64 `json:"roleAssignmentId"`
+}
