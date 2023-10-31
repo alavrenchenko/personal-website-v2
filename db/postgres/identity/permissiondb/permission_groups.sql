3870168b8f6029023833c17044f9d2733afeaec9
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-11-01 02:34:11 +0300

    db.postgres.identity.permissiondb: add permission_group_exists to create_permission_group

--- db/postgres/identity/permissiondb/permission_groups.sql
+++ db/postgres/identity/permissiondb/permission_groups.sql
@@ -26,13 +26,14 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- PROCEDURE: public.create_permission_group(character varying, bigint, text, bigint, text)
+-- PROCEDURE: public.create_permission_group(character varying, bigint, text, bigint, bigint, text)
 /*
 Permission group statuses:
     Active = 2
 
 Error codes:
-    NoError = 0
+    NoError                      = 0
+    PermissionGroupAlreadyExists = 12001
 */
 CREATE OR REPLACE PROCEDURE public.create_permission_group(
     IN _name public.permission_groups.name%TYPE,
@@ -51,11 +52,26 @@ BEGIN
     err_code := 0; -- NoError
     err_msg := '';
 
+    IF public.permission_group_exists(_name) THEN
+        err_code := 12001; -- PermissionGroupAlreadyExists
+        err_msg := 'permission group with the same name already exists';
+        RETURN;
+    END IF;
+
     _time := (clock_timestamp() AT TIME ZONE 'UTC');
     -- permission group status: Active(2)
     INSERT INTO public.permission_groups(name, created_at, created_by, updated_at, updated_by, status, status_updated_at, status_updated_by,
             status_comment, app_id, app_group_id, description, _version_stamp, _timestamp)
         VALUES (_name, _time, _created_by, _time, _created_by, 2, _time, _created_by, _status_comment, _app_id, _app_group_id, _description, 1, _time)
         RETURNING id INTO _id;
+
+    EXCEPTION
+        WHEN unique_violation THEN
+            IF public.permission_group_exists(_name) THEN
+                err_code := 12001; -- PermissionGroupAlreadyExists
+                err_msg := 'permission group with the same name already exists';
+                RETURN;
+            END IF;
+            RAISE;
 END;
 $$ LANGUAGE plpgsql;
