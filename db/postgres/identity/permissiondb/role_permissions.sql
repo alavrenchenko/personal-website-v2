a6d7698b602633bc4853ecdd37ad9f484dc55599
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-11-01 20:58:11 +0300

    db.postgres.identity.permissiondb: add revoke_permissions

--- db/postgres/identity/permissiondb/role_permissions.sql
+++ db/postgres/identity/permissiondb/role_permissions.sql
@@ -55,7 +55,7 @@ Error codes:
     InvalidOperation         = 3
     InvalidData              = 1000
     PermissionNotFound       = 11800
-    PermissionAlreadyGranted = 11900
+    PermissionAlreadyGranted = 11902
 */
 -- Minimum transaction isolation level: Serializable.
 CREATE OR REPLACE PROCEDURE public.grant_permissions(
@@ -96,22 +96,77 @@ BEGIN
         END IF;
 
         IF public.is_permission_granted(_role_id, _permission_id) THEN
-            err_code := 11900; -- PermissionAlreadyGranted
+            err_code := 11902; -- PermissionAlreadyGranted
             err_msg := format('permission (%s) already granted to the role', _permission_id);
             RETURN;
         END IF;
 
-        INSERT INTO public.role_assignments(role_id, permission_id, created_at, created_by, _version_stamp, _timestamp)
+        INSERT INTO public.role_permissions(role_id, permission_id, created_at, created_by, _version_stamp, _timestamp)
             VALUES (_role_id, _permission_id, _time, _operation_user_id, 1, _time);
     END LOOP;
 
     EXCEPTION
         WHEN unique_violation THEN
             IF _permission_id IS NOT NULL AND public.is_permission_granted(_role_id, _permission_id) THEN
-                err_code := 11900; -- PermissionAlreadyGranted
+                err_code := 11902; -- PermissionAlreadyGranted
                 err_msg := format('permission (%s) already granted to the role', _permission_id);
                 RETURN;
             END IF;
             RAISE;
+END;
+$$ LANGUAGE plpgsql;
+
+-- PROCEDURE: public.revoke_permissions(bigint, bigint[], bigint)
+/*
+Error codes:
+    NoError              = 0
+    InternalError        = 2
+    InvalidData          = 1000
+    PermissionNotGranted = 11903
+*/
+-- Minimum transaction isolation level: Read committed.
+CREATE OR REPLACE PROCEDURE public.revoke_permissions(
+    IN _role_id public.role_permissions.role_id%TYPE,
+    IN _permission_ids bigint[],
+    IN _operation_user_id public.role_permissions.deleted_by%TYPE,
+    OUT err_code bigint,
+    OUT err_msg text) AS $$
+DECLARE
+    _time timestamp(6) without time zone;
+    _permission_id bigint;
+    _id public.role_permissions.id%TYPE;
+BEGIN
+    err_code := 0; -- NoError
+    err_msg := '';
+
+    IF array_length(_permission_ids, 1) IS NULL THEN
+        err_code := 1000; -- InvalidData
+        err_msg := 'number of permission ids is 0';
+        RETURN;
+    END IF;
+
+    _time := (clock_timestamp() AT TIME ZONE 'UTC');
+
+    -- _permission_ids need to be sorted to avoid deadlocks
+    FOR _permission_id IN SELECT DISTINCT unnest(_permission_ids) ORDER BY 1 LOOP
+        SELECT id INTO _id FROM public.role_permissions WHERE role_id = _role_id AND permission_id = _permission_id AND is_deleted IS FALSE LIMIT 1 FOR UPDATE;
+        IF NOT FOUND THEN
+            err_code := 11903; -- PermissionNotGranted
+            err_msg := format('permission (%s) not granted to the role', _permission_id);
+            RETURN;
+        END IF;
+
+        UPDATE public.role_permissions
+            SET is_deleted = TRUE, deleted_at = _time, deleted_by = _operation_user_id, _version_stamp = _version_stamp + 1, _timestamp = _time
+            WHERE id = _id;
+
+        INSERT INTO public.deleted_role_permissions SELECT * FROM public.role_permissions WHERE id = _id LIMIT 1;
+        -- an insertion check
+        IF NOT FOUND THEN
+            err_code := 2; -- InternalError
+            err_msg := 'role permission info wasn''t inserted into deleted_role_permissions';
+            RETURN;
+        END IF;
+    END LOOP;
 END;
 $$ LANGUAGE plpgsql;
