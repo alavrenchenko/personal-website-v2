ae1e88c4e4905bcfcf66b55085f13844702828d7
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-11-02 14:01:00 +0300

    db.postgres.identity.permissiondb: add revoke_all_permissions

--- db/postgres/identity/permissiondb/role_permissions.sql
+++ db/postgres/identity/permissiondb/role_permissions.sql
@@ -170,3 +170,44 @@ BEGIN
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
+
+-- PROCEDURE: public.revoke_all_permissions(bigint, bigint)
+/*
+Error codes:
+    NoError       = 0
+    InternalError = 2
+*/
+-- Minimum transaction isolation level: Read committed.
+CREATE OR REPLACE PROCEDURE public.revoke_all_permissions(
+    IN _role_id public.role_permissions.role_id%TYPE,
+    IN _operation_user_id public.role_permissions.deleted_by%TYPE,
+    OUT err_code bigint,
+    OUT err_msg text) AS $$
+DECLARE
+    _time timestamp(6) without time zone;
+    -- rows need to be sorted to avoid deadlocks
+    _rpc CURSOR FOR
+        SELECT id FROM public.role_permissions
+            WHERE role_id = _role_id AND is_deleted IS FALSE
+            ORDER BY permission_id
+            FOR UPDATE;
+BEGIN
+    err_code := 0; -- NoError
+    err_msg := '';
+    _time := (clock_timestamp() AT TIME ZONE 'UTC');
+
+    FOR _item IN _rpc LOOP
+        UPDATE public.role_permissions
+            SET is_deleted = TRUE, deleted_at = _time, deleted_by = _operation_user_id, _version_stamp = _version_stamp + 1, _timestamp = _time
+            WHERE CURRENT OF _rpc;
+
+        INSERT INTO public.deleted_role_permissions SELECT * FROM public.role_permissions WHERE id = _item.id LIMIT 1;
+        -- an insertion check
+        IF NOT FOUND THEN
+            err_code := 2; -- InternalError
+            err_msg := 'role permission info wasn''t inserted into deleted_role_permissions';
+            RETURN;
+        END IF;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
