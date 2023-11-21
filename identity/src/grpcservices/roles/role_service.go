6b5f2aa633f20d3a5b50d9c0cb818f5c97eb7fc4
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-11-21 18:04:03 +0300

    identity.grpcservices.roles: add GetAllByNames to RoleService

--- identity/src/grpcservices/roles/role_service.go
+++ identity/src/grpcservices/roles/role_service.go
@@ -15,12 +15,23 @@
 package roles
 
 import (
+	"context"
 	"fmt"
 
+	"google.golang.org/grpc/codes"
+
 	rolespb "personal-website-v2/go-apis/identity/roles"
+	iapierrors "personal-website-v2/identity/src/api/errors"
+	"personal-website-v2/identity/src/api/grpc/roles/converter"
+	rolevalidation "personal-website-v2/identity/src/api/grpc/roles/validation/roles"
 	iactions "personal-website-v2/identity/src/internal/actions"
+	ierrors "personal-website-v2/identity/src/internal/errors"
+	"personal-website-v2/identity/src/internal/logging/events"
 	"personal-website-v2/identity/src/internal/roles"
 	"personal-website-v2/pkg/actions"
+	apierrors "personal-website-v2/pkg/api/errors"
+	apigrpcerrors "personal-website-v2/pkg/api/grpc/errors"
+	"personal-website-v2/pkg/errors"
 	grpcserverhelper "personal-website-v2/pkg/helper/net/grpc/server"
 	"personal-website-v2/pkg/logging"
 	lcontext "personal-website-v2/pkg/logging/context"
@@ -59,4 +70,48 @@ func NewRoleService(
 		roleManager:  roleManager,
 		logger:       l,
 	}, nil
+}
+
+// GetAllByNames gets all roles by the specified role names.
+func (s *RoleService) GetAllByNames(ctx context.Context, req *rolespb.GetAllByNamesRequest) (*rolespb.GetAllByNamesResponse, error) {
+	var res *rolespb.GetAllByNamesResponse
+	err := s.reqProcessor.Process(ctx, iactions.ActionTypeRole_GetAllByNames, iactions.OperationTypeRoleService_GetAllByNames,
+		func(opCtx *actions.OperationContext) error {
+			if err := rolevalidation.ValidateGetAllByNamesRequest(req); err != nil {
+				s.logger.ErrorWithEvent(opCtx.CreateLogEntryContext(), events.GrpcServices_RoleServiceEvent, nil,
+					"[roles.RoleService.GetAllByNames] "+err.Message(),
+				)
+				return apigrpcerrors.CreateGrpcError(codes.InvalidArgument, err)
+			}
+
+			rs, err := s.roleManager.GetAllByNames(opCtx, req.Names)
+			if err != nil {
+				s.logger.ErrorWithEvent(opCtx.CreateLogEntryContext(), events.GrpcServices_RoleServiceEvent, err,
+					"[roles.RoleService.GetAllByNames] get all roles by names",
+				)
+
+				if err2 := errors.Unwrap(err); err2 != nil {
+					switch err2.Code() {
+					case errors.ErrorCodeInvalidData:
+						return apigrpcerrors.CreateGrpcError(codes.InvalidArgument, apierrors.NewApiError(apierrors.ApiErrorCodeInvalidData, err2.Message()))
+					case ierrors.ErrorCodeRoleNotFound:
+						return apigrpcerrors.CreateGrpcError(codes.NotFound, apierrors.NewApiError(iapierrors.ApiErrorCodeRoleNotFound, err2.Message()))
+					}
+				}
+				return apigrpcerrors.CreateGrpcError(codes.Internal, apierrors.ErrInternal)
+			}
+
+			rs2 := make([]*rolespb.Role, len(rs))
+			for i := 0; i < len(rs); i++ {
+				rs2[i] = converter.ConvertToApiRole(rs[i])
+			}
+
+			res = &rolespb.GetAllByNamesResponse{Roles: rs2}
+			return nil
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
