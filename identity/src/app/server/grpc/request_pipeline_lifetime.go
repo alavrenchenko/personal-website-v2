cb9607fb547e1f45bbdbca21e1d1aebacdc70c35
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-11-28 19:36:46 +0300

    identity.app.server.grpc: add IdentityManager to RequestPipelineLifetime (Authenticate)

--- identity/src/app/server/grpc/request_pipeline_lifetime.go
+++ identity/src/app/server/grpc/request_pipeline_lifetime.go
@@ -15,6 +15,7 @@
 package grpc
 
 import (
+	"context"
 	"fmt"
 	"time"
 
@@ -26,19 +27,20 @@ import (
 	"personal-website-v2/pkg/api/metadata"
 	"personal-website-v2/pkg/app"
 	"personal-website-v2/pkg/base/nullable"
+	grpcserverhelper "personal-website-v2/pkg/helper/net/grpc/server"
 	"personal-website-v2/pkg/identity"
-	"personal-website-v2/pkg/identity/account"
 	"personal-website-v2/pkg/logging"
-	"personal-website-v2/pkg/logging/context"
+	lcontext "personal-website-v2/pkg/logging/context"
 	"personal-website-v2/pkg/logging/events"
 	"personal-website-v2/pkg/net/grpc/server"
 )
 
 type RequestPipelineLifetime struct {
-	appSessionId  uint64
-	tranManager   *actions.TransactionManager
-	actionManager *actions.ActionManager
-	logger        logging.Logger[*context.LogEntryContext]
+	appSessionId    uint64
+	tranManager     *actions.TransactionManager
+	reqProcessor    *grpcserverhelper.RequestProcessor
+	identityManager identity.IdentityManager
+	logger          logging.Logger[*lcontext.LogEntryContext]
 }
 
 var _ server.RequestPipelineLifetime = (*RequestPipelineLifetime)(nil)
@@ -47,18 +49,30 @@ func NewRequestPipelineLifetime(
 	appSessionId uint64,
 	tranManager *actions.TransactionManager,
 	actionManager *actions.ActionManager,
-	loggerFactory logging.LoggerFactory[*context.LogEntryContext]) (*RequestPipelineLifetime, error) {
+	identityManager identity.IdentityManager,
+	loggerFactory logging.LoggerFactory[*lcontext.LogEntryContext],
+) (*RequestPipelineLifetime, error) {
 	l, err := loggerFactory.CreateLogger("app.server.grpc.RequestPipelineLifetime")
-
 	if err != nil {
-		return nil, fmt.Errorf("[http.NewRequestPipelineLifetime] create a logger: %w", err)
+		return nil, fmt.Errorf("[grpc.NewRequestPipelineLifetime] create a logger: %w", err)
+	}
+
+	c := &grpcserverhelper.RequestProcessorConfig{
+		ActionGroup:    actions.ActionGroupNetGrpcServer_RequestPipelineLifetime,
+		OperationGroup: actions.OperationGroupNetGrpcServer_RequestPipelineLifetime,
+		StopAppIfError: true,
+	}
+	p, err := grpcserverhelper.NewRequestProcessor(appSessionId, actionManager, c, loggerFactory)
+	if err != nil {
+		return nil, fmt.Errorf("[grpc.NewRequestPipelineLifetime] new request processor: %w", err)
 	}
 
 	return &RequestPipelineLifetime{
-		appSessionId:  appSessionId,
-		tranManager:   tranManager,
-		actionManager: actionManager,
-		logger:        l,
+		appSessionId:    appSessionId,
+		tranManager:     tranManager,
+		reqProcessor:    p,
+		identityManager: identityManager,
+		logger:          l,
 	}, nil
 }
 
@@ -69,10 +83,9 @@ func (l *RequestPipelineLifetime) BeginRequest(ctx *server.GrpcContext) error {
 
 	if len(opCtxVal) > 0 {
 		opCtx, err := metadata.DecodeOperationContextFromString(opCtxVal[0])
-
 		if err != nil {
 			l.logger.ErrorWithEvent(
-				&context.LogEntryContext{AppSessionId: nullable.NewNullable(l.appSessionId)},
+				&lcontext.LogEntryContext{AppSessionId: nullable.NewNullable(l.appSessionId)},
 				events.NetGrpcServer_RequestPipelineLifetimeEvent,
 				err,
 				"[grpc.RequestPipelineLifetime.BeginRequest] decode OperationContext from string",
@@ -85,9 +98,8 @@ func (l *RequestPipelineLifetime) BeginRequest(ctx *server.GrpcContext) error {
 		t = actions.NewTransaction(opCtx.TransactionId, time.Time{})
 	} else {
 		t, err = l.tranManager.CreateAndStart()
-
 		if err != nil {
-			leCtx := &context.LogEntryContext{AppSessionId: nullable.NewNullable(l.appSessionId)}
+			leCtx := &lcontext.LogEntryContext{AppSessionId: nullable.NewNullable(l.appSessionId)}
 			l.logger.FatalWithEventAndError(
 				leCtx,
 				events.NetGrpcServer_RequestPipelineLifetimeEvent,
@@ -113,7 +125,6 @@ func (l *RequestPipelineLifetime) BeginRequest(ctx *server.GrpcContext) error {
 		"[grpc.RequestPipelineLifetime.BeginRequest] grpc request and transaction initialized",
 		logging.NewField("callId", ctx.CallId()),
 	)
-
 	if err != nil {
 		go func() {
 			if err2 := app.Stop(); err2 != nil {
@@ -126,8 +137,25 @@ func (l *RequestPipelineLifetime) BeginRequest(ctx *server.GrpcContext) error {
 }
 
 func (l *RequestPipelineLifetime) Authenticate(ctx *server.GrpcContext) error {
-	ctx.User = identity.NewDefaultIdentity(nullable.Nullable[uint64]{}, account.UserGroupAnonymousUsers, nullable.Nullable[uint64]{})
-	return nil
+	if ctx.IncomingOperationCtx == nil || !ctx.IncomingOperationCtx.UserId.HasValue && !ctx.IncomingOperationCtx.ClientId.HasValue {
+		ctx.User = identity.NewDefaultIdentity(nullable.Nullable[uint64]{}, identity.UserTypeUser, nullable.Nullable[uint64]{})
+		return nil
+	}
+	return l.reqProcessor.Process(server.NewIncomingContextWithGrpcContext(context.Background(), ctx),
+		actions.ActionTypeNetGrpcServer_RequestPipelineLifetime_Authenticate, actions.OperationTypeNetGrpcServer_RequestPipelineLifetime_Authenticate,
+		func(opCtx *actions.OperationContext) error {
+			i, err := l.identityManager.AuthenticateById(opCtx, ctx.IncomingOperationCtx.UserId, ctx.IncomingOperationCtx.ClientId)
+			if err != nil {
+				l.logger.ErrorWithEvent(opCtx.CreateLogEntryContext(), events.NetGrpcServer_RequestPipelineLifetimeEvent, err,
+					"[grpc.RequestPipelineLifetime.Authenticate] authenticate a user and a client by id",
+				)
+				return apigrpcerrors.CreateGrpcError(codes.Internal, apierrors.ErrInternal)
+			}
+
+			ctx.User = i
+			return nil
+		},
+	)
 }
 
 func (l *RequestPipelineLifetime) Authorize(ctx *server.GrpcContext) error {
@@ -147,16 +175,16 @@ func (l *RequestPipelineLifetime) Error(ctx *server.GrpcContext, err error) {
 func (l *RequestPipelineLifetime) EndRequest(ctx *server.GrpcContext) {
 	l.logger.InfoWithEvent(
 		l.createLogEntryContext(ctx.Transaction),
-		events.NetHttpServer_RequestPipelineLifetimeEvent,
+		events.NetGrpcServer_RequestPipelineLifetimeEvent,
 		"[grpc.RequestPipelineLifetime.EndRequest] end a request",
 		logging.NewField("callId", ctx.CallId()),
 	)
 }
 
-func (l *RequestPipelineLifetime) createLogEntryContext(tran *actions.Transaction) *context.LogEntryContext {
-	return &context.LogEntryContext{
+func (l *RequestPipelineLifetime) createLogEntryContext(tran *actions.Transaction) *lcontext.LogEntryContext {
+	return &lcontext.LogEntryContext{
 		AppSessionId: nullable.NewNullable(l.appSessionId),
-		Transaction: &context.TransactionInfo{
+		Transaction: &lcontext.TransactionInfo{
 			Id: tran.Id(),
 		},
 	}
