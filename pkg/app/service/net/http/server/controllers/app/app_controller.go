8eb43cb72f1d4eb55502823bb92ed6a23ba3f601
Alexey Lavrenchenko <laa5550509@gmail.com>
2023-11-30 09:01:05 +0300

    pkg.app.service.net.http.server.controllers.app: replace reqProcessor.Process with reqProcessor.ProcessWithAuthz

--- pkg/app/service/net/http/server/controllers/app/app_controller.go
+++ pkg/app/service/net/http/server/controllers/app/app_controller.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 
 	"personal-website-v2/pkg/actions"
-	"personal-website-v2/pkg/api/errors"
 	apihttp "personal-website-v2/pkg/api/http"
 	"personal-website-v2/pkg/app"
 	httpserverhelper "personal-website-v2/pkg/helper/net/http/server"
@@ -34,11 +33,10 @@ type ApplicationControllerIdentityConfig struct {
 }
 
 type ApplicationController struct {
-	app             app.Application
-	reqProcessor    *httpserverhelper.RequestProcessor
-	identityManager identity.IdentityManager
-	identityConfig  *ApplicationControllerIdentityConfig
-	logger          logging.Logger[*lcontext.LogEntryContext]
+	app            app.Application
+	reqProcessor   *httpserverhelper.RequestProcessor
+	identityConfig *ApplicationControllerIdentityConfig
+	logger         logging.Logger[*lcontext.LogEntryContext]
 }
 
 func NewApplicationController(
@@ -59,17 +57,16 @@ func NewApplicationController(
 		OperationGroup: actions.OperationGroupApplication,
 		StopAppIfError: true,
 	}
-	p, err := httpserverhelper.NewRequestProcessor(appSessionId, actionManager, c, loggerFactory)
+	p, err := httpserverhelper.NewRequestProcessor(appSessionId, actionManager, identityManager, c, loggerFactory)
 	if err != nil {
 		return nil, fmt.Errorf("[http.NewRequestPipelineLifetime] new request processor: %w", err)
 	}
 
 	return &ApplicationController{
-		app:             a,
-		reqProcessor:    p,
-		identityManager: identityManager,
-		identityConfig:  identityConfig,
-		logger:          l,
+		app:            a,
+		reqProcessor:   p,
+		identityConfig: identityConfig,
+		logger:         l,
 	}, nil
 }
 
@@ -77,36 +74,19 @@ func NewApplicationController(
 //
 //	[POST] /private/api/app/stop
 func (c *ApplicationController) Stop(ctx *server.HttpContext) {
-	c.reqProcessor.Process(ctx, actions.ActionTypeApplication_Stop, actions.OperationTypeApplicationController_Stop,
+	c.reqProcessor.ProcessWithAuthz(ctx, actions.ActionTypeApplication_Stop, actions.OperationTypeApplicationController_Stop,
+		[]string{c.identityConfig.StopPermission},
 		func(opCtx *actions.OperationContext) bool {
-			leCtx := opCtx.CreateLogEntryContext()
-
-			if authorized, err := c.identityManager.Authorize(opCtx, ctx.User, []string{c.identityConfig.StopPermission}); err != nil {
-				c.logger.ErrorWithEvent(leCtx, events.HttpControllers_ApplicationControllerEvent, err, "[app.ApplicationController.Stop] authorize a user")
-
-				if err = apihttp.InternalServerError(ctx); err != nil {
-					c.logger.ErrorWithEvent(leCtx, events.HttpControllers_ApplicationControllerEvent, err, "[app.ApplicationController.Stop] write InternalServerError")
-				}
-				return false
-			} else if !authorized {
-				c.logger.ErrorWithEvent(opCtx.CreateLogEntryContext(), events.HttpControllers_ApplicationControllerEvent, nil, "[app.ApplicationController.Stop] user not authorized")
-
-				if err = apihttp.Forbidden(ctx, errors.ErrPermissionDenied); err != nil {
-					c.logger.ErrorWithEvent(leCtx, events.HttpControllers_ApplicationControllerEvent, err, "[app.ApplicationController.Stop] write Forbidden")
-				}
-				return false
-			}
-
 			go func() {
 				if err := c.app.StopWithContext(opCtx); err != nil {
-					c.logger.ErrorWithEvent(leCtx, events.HttpControllers_ApplicationControllerEvent, err, "[app.ApplicationController.Stop] stop an app")
+					c.logger.ErrorWithEvent(opCtx.CreateLogEntryContext(), events.HttpControllers_ApplicationControllerEvent, err, "[app.ApplicationController.Stop] stop an app")
 				}
 			}()
 
 			ctx.Response.Writer.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
 
 			if err := apihttp.Ok(ctx, true); err != nil {
-				c.logger.ErrorWithEvent(leCtx, events.HttpControllers_ApplicationControllerEvent, err, "[app.ApplicationController.Stop] write Ok")
+				c.logger.ErrorWithEvent(opCtx.CreateLogEntryContext(), events.HttpControllers_ApplicationControllerEvent, err, "[app.ApplicationController.Stop] write Ok")
 				return false
 			}
 			return true
