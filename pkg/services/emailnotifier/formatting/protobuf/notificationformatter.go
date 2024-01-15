fdd6ffd82df9c4c693b72792c6575b8f34d93b3c
Alexey Lavrenchenko <laa5550509@gmail.com>
2024-01-15 21:23:21 +0300

    pkg.services.emailnotifier: add formatting.protobuf (NotificationFormatter)

---
+++ pkg/services/emailnotifier/formatting/protobuf/notificationformatter.go
@@ -0,0 +1,58 @@
+// Copyright 2024 Alexey Lavrenchenko. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package protobuf
+
+import (
+	"fmt"
+
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	emailnotifierpb "personal-website-v2/go-data/services/emailnotifier"
+	"personal-website-v2/pkg/services/emailnotifier/formatting"
+	"personal-website-v2/pkg/services/emailnotifier/models"
+)
+
+type NotificationFormatter struct {
+	ctx *formatting.FormatterContext
+}
+
+func NewNotificationFormatter(ctx *formatting.FormatterContext) *NotificationFormatter {
+	return &NotificationFormatter{
+		ctx: ctx,
+	}
+}
+
+func (f *NotificationFormatter) Format(n *models.Notification) ([]byte, error) {
+	op := &emailnotifierpb.Notification{
+		Id:         n.Id.String(),
+		CreatedAt:  timestamppb.New(n.CreatedAt),
+		CreatedBy:  n.CreatedBy,
+		Recipients: n.Recipients,
+		Subject:    n.Subject,
+		Body:       n.Body,
+		Metadata: &emailnotifierpb.NotificationMetadata{
+			AppSessionId:    f.ctx.AppSessionId,
+			EmailNotifierId: uint32(f.ctx.EmailNotifierId),
+			TranId:          n.Metadata.TranId.String(),
+		},
+	}
+
+	b, err := proto.Marshal(op)
+	if err != nil {
+		return nil, fmt.Errorf("[protobuf.NotificationFormatter.Format] marshal a notification to Protobuf: %w", err)
+	}
+	return b, nil
+}
