7e4b9eca3b3ccc4a54b147c307e1fde90c3112f6
Alexey Lavrenchenko <laa5550509@gmail.com>
2024-01-15 21:21:36 +0300

    pkg.services.emailnotifier: move Notification, NotificationMetadata to models

---
+++ pkg/services/emailnotifier/models/models.go
@@ -0,0 +1,51 @@
+// Copyright 2024 Alexey Lavrenchenko. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// The notification.
+type Notification struct {
+	// The unique ID to identify the notification.
+	Id uuid.UUID
+
+	// It stores the date and time at which the notification was created.
+	CreatedAt time.Time
+
+	// The user ID to identify the user who created the notification.
+	CreatedBy uint64
+
+	// The notification recipients.
+	Recipients []string
+
+	// The notification subject.
+	Subject string
+
+	// The notification body.
+	Body []byte
+
+	// The notification metadata.
+	Metadata *NotificationMetadata
+}
+
+// The notification metadata.
+type NotificationMetadata struct {
+	// The transaction ID.
+	TranId uuid.UUID
+}
