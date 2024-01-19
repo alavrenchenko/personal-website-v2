95d1a6ed20bc3625bcd3d70cacc9b6ea1d0547e6
Alexey Lavrenchenko <laa5550509@gmail.com>
2024-01-19 05:40:35 +0300

    pkg.components.kafka: add metadata

---
+++ pkg/components/kafka/metadata/metadata.go
@@ -0,0 +1,42 @@
+// Copyright 2024 Alexey Lavrenchenko. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package metadata
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+
+	"personal-website-v2/pkg/components/kafka"
+)
+
+const (
+	MessageIdMDKey = "md_msgid"
+)
+
+func MessageIdHeader(msgId uuid.UUID) *kafka.RecordHeader {
+	return &kafka.RecordHeader{
+		Key:   []byte(MessageIdMDKey),
+		Value: []byte(msgId.String()),
+	}
+}
+
+func DecodeMessageIdHeaderValue(encodedMsgId []byte) (uuid.UUID, error) {
+	id, err := uuid.ParseBytes(encodedMsgId)
+	if err != nil {
+		return uuid.UUID{}, fmt.Errorf("[metadata.DecodeMessageIdHeaderValue] parse encodedMsgId: %w", err)
+	}
+	return id, nil
+}
