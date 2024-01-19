081c8fe4d83211c0eda8a7feeff73844b1f702e4
Alexey Lavrenchenko <laa5550509@gmail.com>
2024-01-19 05:42:53 +0300

    pkg.components.kafka: add utils

---
+++ pkg/components/kafka/utils/sarama/util.go
@@ -0,0 +1,32 @@
+// Copyright 2024 Alexey Lavrenchenko. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sarama
+
+import (
+	"unsafe"
+
+	"github.com/IBM/sarama"
+)
+
+func GetHeader(headers []*sarama.RecordHeader, key string) *sarama.RecordHeader {
+	for i := 0; i < len(headers); i++ {
+		h := headers[i]
+		k := unsafe.String(unsafe.SliceData(h.Key), len(h.Key))
+		if k == key {
+			return h
+		}
+	}
+	return nil
+}
