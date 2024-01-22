43b97eb02fce0d89070f372fc510180746dd295e
Alexey Lavrenchenko <laa5550509@gmail.com>
2024-01-22 20:25:33 +0300

    email-notifier.internal: add errors

---
+++ email-notifier/src/internal/errors/errors.go
@@ -0,0 +1,42 @@
+// Copyright 2024 Alexey Lavrenchenko. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package errors
+
+import "personal-website-v2/pkg/errors"
+
+// Error codes: "personal-website-v2/pkg/errors"
+// Internal error codes (2-9999).
+// Common error codes (10000-19999).
+// reserved error codes: 20000-29999
+
+const (
+	// Notification error codes (31000-31199).
+	ErrorCodeNotificationNotFound      errors.ErrorCode = 31000
+	ErrorCodeNotificationAlreadyExists errors.ErrorCode = 31001
+
+	// Notification group error codes (31200-31399).
+	ErrorCodeNotificationGroupNotFound      errors.ErrorCode = 31200
+	ErrorCodeNotificationGroupAlreadyExists errors.ErrorCode = 31201
+)
+
+var (
+	// Notification errors.
+	ErrNotificationNotFound      = errors.NewError(ErrorCodeNotificationNotFound, "notification not found")
+	ErrNotificationAlreadyExists = errors.NewError(ErrorCodeNotificationAlreadyExists, "notification with the same id already exists")
+
+	// Notification group errors.
+	ErrNotificationGroupNotFound      = errors.NewError(ErrorCodeNotificationGroupNotFound, "notification group not found")
+	ErrNotificationGroupAlreadyExists = errors.NewError(ErrorCodeNotificationGroupAlreadyExists, "notification group with the same name already exists")
+)
