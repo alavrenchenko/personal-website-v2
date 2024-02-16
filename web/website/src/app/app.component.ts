1bc4a3b87a54ad7208987f4220c024957aa0fed3
Alexey Lavrenchenko <laa5550509@gmail.com>
2024-02-16 23:46:38 +0300

    web.website.app: add onWindowClick to AppComponent

--- web/website/src/app/app.component.ts
+++ web/website/src/app/app.component.ts
@@ -21,7 +21,7 @@ import { Router } from "@angular/router";
 import { IdentityService } from "../../../pkg/identity";
 import { NavigationFocus } from "./core/navigation/navigation-focus";
 import { PageBodyComponent, PageFooterComponent, PageHeaderComponent } from "./page";
-import { GoogleAnalyticsService, formatErrorEventForGAnalytics, formatErrorForGAnalytics } from "../../../pkg/analytics/google";
+import { GoogleAnalyticsEventName, GoogleAnalyticsService, formatErrorEventForGAnalytics } from "../../../pkg/analytics/google";
 
 @Component({
     selector: "app-root",
@@ -43,6 +43,7 @@ export class AppComponent implements OnInit, OnDestroy {
         router: Router
     ) {
         this._navFocus = new NavigationFocus(router);
+
     }
 
     ngOnInit(): void {
@@ -50,13 +51,61 @@ export class AppComponent implements OnInit, OnDestroy {
             this._identityService.init();
         });
 
+        this.onWindowClick = this.onWindowClick.bind(this);
+        window.addEventListener('click', this.onWindowClick, true);
     }
 
     ngOnDestroy(): void {
         this._navFocus.dispose();
+        window.removeEventListener('click', this.onWindowClick, true);
     }
 
     onWindowError(e: ErrorEvent): void {
         this._analytics.sendError(formatErrorEventForGAnalytics(e), true);
+    }
+
+    onWindowClick(e: MouseEvent): void {
+        const params: { [key: string]: any; } = {
+            // metadata
+            md_location: window.location.href,
+            md_user_agent: window.navigator.userAgent
+        };
+
+        if (e.target instanceof HTMLElement) {
+            let outerHTML = e.target.outerHTML;
+            if (outerHTML.length > 100) {
+                outerHTML = `${outerHTML.substring(0, 100)}...`;
+            }
+
+            params.target_class_name = e.target.className;
+            params.target_outer_html = outerHTML;
+            params.target_tag_name = e.target.tagName;
+
+            if (e.target instanceof HTMLAnchorElement) {
+                params.target_href = e.target.href;
+            } else if (e.target instanceof HTMLImageElement) {
+                params.target_current_src = e.target.currentSrc;
+            } else if (e.target instanceof HTMLInputElement) {
+                params.target_name = e.target.name;
+            }
+
+            const parentElem = e.target.parentElement;
+            if (parentElem) {
+                let innerHTML = parentElem.innerHTML;
+                if (innerHTML.length > 100) {
+                    innerHTML = `${innerHTML.substring(0, 100)}...`;
+                }
+
+                params.target_parent_elem_class_name = parentElem.className;
+                params.target_parent_elem_inner_html = innerHTML;
+                params.target_parent_elem_tag_name = parentElem.tagName;
+
+                if (parentElem instanceof HTMLAnchorElement) {
+                    params.target_parent_elem_href = parentElem.href;
+                }
+            }
+        }
+
+        this._analytics.sendEvent(GoogleAnalyticsEventName.WINDOW_CLICK, params);
     }
 }
