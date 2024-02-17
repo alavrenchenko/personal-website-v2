127256ca909e97e9ffc4e78fdf56fe23bdb33c40
Alexey Lavrenchenko <laa5550509@gmail.com>
2024-02-17 05:17:42 +0300

    web.website.app: add HTMLTextAreaElement to AppComponent.onWindowClick

--- web/website/src/app/app.component.ts
+++ web/website/src/app/app.component.ts
@@ -87,6 +87,8 @@ export class AppComponent implements OnInit, OnDestroy {
                 params.target_current_src = e.target.currentSrc;
             } else if (e.target instanceof HTMLInputElement) {
                 params.target_name = e.target.name;
+            } else if (e.target instanceof HTMLTextAreaElement) {
+                params.target_name = e.target.name;
             }
 
             const parentElem = e.target.parentElement;
